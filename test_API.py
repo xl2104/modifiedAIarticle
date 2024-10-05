a0587b1e9b1e32dd168a9ba1ff23b506ffe86291
Sheena Liu <sheenaliu@Sheenas-Air.lan>
2024-10-04 21:20:26 -0400

    Changed back all sources allowed

--- test_API.py
+++ test_API.py
@@ -20,7 +20,7 @@ origins = [
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # ["*"],  # Allows all origins
+    allow_origins=["*"],  # ["*"],origins,  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
